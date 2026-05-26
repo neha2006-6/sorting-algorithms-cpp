2dd9df59ebf8844117d342c45d1268fa0a90d600
Neha Megharaja Gujaran <neha.mgujaran@gmail.com>
2026-05-26 09:21:19 +0530

    Implement bubble sort algorithm in bubble.cpp

---
+++ bubble.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  
+int n;
+
+cout << "Enter number of element" ;
+cin>>n;
+
+  int a[n];
+
+cout<<"Enter array of element ";
+
+for(int i=0;i<n;i++)
+{
+cin>>a[i];
+  }
+
+
+for(int i=0;i<n-1;i++)
+{
+for(int j=0;j<n-i-1;j++)
+{
+if(a[j]>a[j+1])
+{
+int temp=a[j];
+a[j]=a[j+1];
+a[j+1]=temp;
+  }
+}
+}
+ cout<<"Sorted array ";
+for(int i=0;i<n;i++)
+{
+cout<< a[i] <<" ";
+  }
+return 0;
+}
+
+  
